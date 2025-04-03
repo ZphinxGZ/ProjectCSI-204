d6db87ffdf98aa159a2dc687edea6f86c40c2363
Nitiphon <nitphon.udo@spumail.net>
2025-04-04 04:12:37 +0700

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App"; // Import App component
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import ProcurementOrders from "./pages/ProcurementOrders";
-import OrderDetails from "./components/OrderDetails";
-import ProcurementPayments from "./pages/ProcurementPayments";
-import ProcurementWarehouse from "./pages/ProcurementWarehouse";
-import ProcurementPR from "./pages/ProcurementPR";
-import UserAccounts from "./pages/UserAccounts";
-import UserRoles from "./pages/UserRoles";
-import UserAudit from "./pages/UserAudit";
-import UserSecurity from "./pages/UserSecurity";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App> {/* Wrap Routes with App */}
-        <Routes>
-          <Route path="/" element={<Navigate />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path="/procurement/orders" element={<ProcurementOrders />} />
-          <Route path="/order-details" element={<OrderDetails />} />
-          <Route path="/procurement/payments" element={<ProcurementPayments />} />
-          <Route path="/procurement/warehouse" element={<ProcurementWarehouse />} />
-          <Route path="/procurement/pr" element={<ProcurementPR />} />
-          <Route path="/user-management/accounts" element={<UserAccounts />} />
-          <Route path="/user-management/roles" element={<UserRoles />} />
-          <Route path="/user-management/audit" element={<UserAudit />} />
-          <Route path="/user-management/security" element={<UserSecurity />} />
-        </Routes>
-      </App>
+      <App /> {/* App จะจัดการ routing ทั้งหมด */}
     </Router>
   </StrictMode>
 );
